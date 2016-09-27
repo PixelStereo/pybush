8327160b8d58d80e9a8b4c7d012e16cc5e6d5d96
reno- <contact@rubiano.fr>
2016-09-27 19:34:16 +0200

    app 2 devices conversion

--- examples/test-osc.py
+++ examples/test-osc.py
@@ -6,21 +6,21 @@ lib_path = os.path.abspath('./../')
 sys.path.append(lib_path)
 
 #import py
-from pybush.application import application_new, applications
+from pybush.device import device_new, get_devices_list
 
 def headerprint(args):
 	print ('---------------- ' + str(args) + ' ----------------')
 	
-# create the main application
-my_app = application_new('test_App',author='Pixel Stereo',version='0.0.1',project='my first application')
+# create the device
+my_device = device_new('test_App',author='Pixel Stereo',version='0.0.1',project='my first application')
 
 # create two nodes
-node_1 = my_app.node_new('node.1')
-node_2 = my_app.node_new('node.2')
+node_1 = my_device.node_new('node.1')
+node_2 = my_device.node_new('node.2')
 node_3 = node_2.node_new('node.3')
 
 # create a few parameters for app
-param_0 = my_app.parameter_new('param.0',value=-1,datatype='decimal',tags=['uno','dos'],priority=-1,rangeBounds=[0,1],rangeClipmode='both',repetitionsFilter=1)
+param_0 = my_device.parameter_new('param.0',value=-1,datatype='decimal',tags=['uno','dos'],priority=-1,rangeBounds=[0,1],rangeClipmode='both',repetitionsFilter=1)
 # create a few parameters for node_1 and node_2
 param_1 = node_1.parameter_new('param.1',value=-1,datatype='decimal',tags=['uno','dos'],priority=-1,rangeBounds=[0,1],rangeClipmode='both',repetitionsFilter=1)
 param_2 = node_1.parameter_new('param.2',value=-1,datatype='decimal',tags=['uno','dos'],priority=-1,rangeBounds=[0,1],rangeClipmode='both',repetitionsFilter=1)
@@ -41,14 +41,14 @@ def get_app_attr(app):
 
 
 headerprint('Application attributes')
-for attr in get_app_attr(my_app):
-	print (attr + ' : ' + str(getattr(my_app, attr)))
+for attr in get_app_attr(my_device):
+	print (attr + ' : ' + str(getattr(my_device, attr)))
 
 
 headerprint('Namespace Explorer')
-print ('my_app has ' + str(len(my_app.nodes)) + ' nodes')
+print ('my_device has ' + str(len(my_device.nodes)) + ' nodes')
 
-for child in my_app.nodes:
+for child in my_device.nodes:
 	if child.nodes:
 		print(child.name + ' has children : ' + str(child.nodes))
 	else:
@@ -56,5 +56,5 @@ for child in my_app.nodes:
 
 print('----------------------------------------')
 print('----------------------------------------')
-for pop in vars(my_app):
+for pop in vars(my_device):
 	print pop
