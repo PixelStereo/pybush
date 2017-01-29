c663e036e40b2098391997f2662d7e53c49ee6c2
reno- <contact@rubiano.fr>
2017-01-29 02:42:54 +0100

    FIX use device output to send OSC value

--- pybush/state.py
+++ pybush/state.py
@@ -80,26 +80,29 @@ class State(File):
         update is called when value is updated
         might be used to send it to network or other protocols
         """
-        ip_add = 'localhost'
-        udp = 1234
-        try:
-            target = liblo.Address(ip_add, int(udp))
+        for out in self.parent.get_device().outputs:
+            split = out.port.split(':')
+            ip_add = split[0]
+            udp = split[1]
+            try:
+                target = liblo.Address(ip_add, int(udp))
+                if __dbug__ >= 3:
+                    print('connect to : ' + ip_add + ':' + str(udp))
+            except liblo.AddressError as err:
+                if __dbug__ >= 3:
+                    print('liblo.AddressError' + str(err))
+            msg = liblo.Message('/' + self.address)
+            if isinstance(self.value, list):
+                # this is a list of values to send
+                for arg in self.value:
+                    arg = check_type(arg)
+                    msg.add(arg)
+            else:
+                # this is a single value to send
+                msg.add(self.value)
+            liblo.send(target, msg)
             if __dbug__ >= 3:
-                print('connect to : ' + ip_add + ':' + str(udp))
-        except liblo.AddressError as err:
-            if __dbug__ >= 3:
-                print('liblo.AddressError' + str(err))
-        msg = liblo.Message(self.address)
-        if isinstance(self.value, list):
-            # this is just a list of values to send
-            for arg in self.value:
-                arg = check_type(arg)
-                msg.add(arg)
-        else:
-            msg.add(self.value)
-        liblo.send(target, msg)
-        if __dbug__ >= 3:
-            print('update ' + self.name + ' to value ' + str(self.value))
+                print('update ' + self.name + ' to value ' + str(self.value))
 
     @property
     def value(self):
