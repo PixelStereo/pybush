c4068b758bcfaadb12aa048c0df821c98c0763d0
reno- <contact@rubiano.fr>
2017-01-29 02:40:59 +0100

    add a simple exemple with millumin

--- examples/demo.py
+++ examples/demo.py
@@ -5,62 +5,32 @@ import os, sys
 sys.path.append(os.path.abspath('./../'))
 
 from time import sleep
-from pybush import new_device, get_devices
+from pybush import new_device
 
-my_device = new_device(name='My device', author='Pixel Stereo', version='0.1.0')
-another_device = new_device(name='My Other device', author='Stereo Pixel', version='0.1.1')
+my_device = new_device(name='millumin', author='Pixel Stereo', version='0.1.0')
+output = my_device.new_output(protocol='OSC', port='127.0.0.1:5000')
+opacity = my_device.new_parameter({
+									'name':'test/opacity',
+									'value':0.,
+									'tags':['millumin', 'video'],
+									'datatype':'decimal',
+									'domain':[0,1],
+									'clipmode':'both',
+									'unique':True})
 
-for dev in get_devices():
-	print(dev)
-output = my_device.new_output(protocol='OSC', port='127.0.0.1:1234')
-output = my_device.new_output(protocol='MIDI')
-node3 = my_device.new_child(name="node.3")
-param1 = my_device.new_parameter({'name':'param.1'})
-param2 = my_device.new_parameter({'name':'node.1/node.2', 'value':1, 'tags':['init', 'video'], 'datatype':'decimal', 'domain':[0,11], \
-								'clipmode':'both', 'repetitions':True})
-param3 = my_device.new_parameter({'name':'node.1/node.2/node.3', 'value':-0.5, 'datatype':'decimal', \
-								'domain':[-1,1], 'clipmode':'low', 'repetitions':False})
+column = my_device.new_parameter({
+									'name':'action/launchColumn',
+									'value':1,
+									'tags':['millumin', 'video'],
+									'datatype':'integer',
+									'domain':[1,35],
+									'clipmode':'low',
+									'unique':False})
 
-snap_param3 = param3.snap()
-#print(snap_param3)
-param3.clipmode = "both"
-param3.domain = [-1, 1]
-param3.datatype = "decimal"
-param3.unique = True
-param3.value = 0.5
-snap2_param3 = param3.snap()
-#print(snap2_param3)
-#param3.recall(snap_param3)
-#print(param3)
-#param3.recall(snap2_param3)
-#print(param3)
-for snap in param3.snapshots:
-	print(param3.snapshots.index(snap), snap)
-#snap_device = my_device.snap()
-#print('DEVICE SNAPSHOT ', snap_device)
-#snap_project = my_device.snap()
-#print('PROJECT SNAPSHOT', snap_project)
-print(my_device.export())
-print(my_device.write())
-param2.value = 0
-param2.ramp(1, 3000)
-sleep(1.5)
-param2.domain = [0.1, 0.4]
-param2.random(0.1, 3000)
-param3.value = 1
-param3.datatype = 'decimal'
-param3.ramp(0, 500)
-sleep(0.5)
-param3.domain = [0.4, 0.6]
-param3.random(destination=1, duration=700)
-param3.value = 0.7
-param2.ramp(0, 500)
-sleep(0.7)
-param2.value = 1
-print('ramp start')
-my_ramp = param2.ramp(0, 1000)
-sleep(0.5)
-my_ramp.terminate()
-while my_ramp.is_alive():
-	pass
-print('ramping is over')
+opacity.ramp(1, 800)
+sleep(1)
+column.value = 3
+sleep(2)
+column.value = 2
+opacity.ramp(0, 1000)
+sleep(2)
