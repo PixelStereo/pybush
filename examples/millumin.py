36e5eb93075c4d16e1e4db44339902c2d75e50e7
reno- <contact@rubiano.fr>
2017-02-04 15:35:32 +0100

    make a real example with functionality

--- examples/millumin.py
+++ examples/millumin.py
@@ -8,8 +8,8 @@ from time import sleep
 from pybush import new_device
 
 my_device = new_device(name='millumin', author='Pixel Stereo', version='0.1.0')
-output = my_device.new_output(protocol='OSC', port='127.0.0.1:5000')
-opacity = my_device.new_parameter({
+output = my_device.new_output(protocol='OSC', port='127.0.0.1:1234')
+"""opacity = my_device.new_parameter({
 									'name':'Layer/opacity',
 									'value':0.,
 									'tags':['millumin', 'video'],
@@ -17,20 +17,33 @@ opacity = my_device.new_parameter({
 									'domain':[0,1],
 									'clipmode':'both',
 									'unique':True})
+"""
 
+# TODO
+# don't push value when creating parameter
+# make it silent?
 column = my_device.new_parameter({
 									'name':'action/launchColumn',
 									'value':1,
 									'tags':['millumin', 'video'],
 									'datatype':'integer',
-									'domain':[1,35],
+									'domain':[22,35],
 									'clipmode':'low',
 									'unique':False})
 
-opacity.ramp(1, 800)
-sleep(1)
-column.value = 3
-sleep(2)
+
+sleep(0.1)
+# just send a value
 column.value = 2
-opacity.ramp(0, 1000)
-sleep(2)
+
+# make a ramp
+#column.ramp(7, 1000, 200)
+sleep(0.1)
+column.random(2, 1000, 100)
+sleep(1.1)
+# with silent activated, value is internally update,
+# but the value is not spread to the world
+column.silent = True
+column.value = 4
+# send manually the value to the world
+column.update()
